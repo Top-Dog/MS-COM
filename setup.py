abcc5802ce16d5ab00152b2b8d019ecbb0a1662c
Top-Dog <Top-Dog@users.noreply.github.com>
2016-04-22 14:41:16 +1200

    Update setup.py
    Added support for late binding in excel

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+import sys
+from win32com.client import makepy
 
 def readme():
     with open('README.rst') as f:
@@ -14,8 +16,17 @@ if __name__ == "__main__":
 		  author_email='sdo51@uclive.ac.nz',
 		  license='MIT',
 		  packages=['MSOffice', 'MSOffice.Excel.Charts', 'MSOffice.Excel.Worksheets'],
-		  install_requires=['numpy', 'pywin32', 'pyodbc', 'six', 'wheel', 'virtualenv'], #pypiwin32
+		  install_requires=['numpy', 'pypiwin32', 'pyodbc', 'six', 'wheel', 'virtualenv'], #pywin32
 		  dependency_links=[], 
 		  zip_safe=False,
 		  #packages=find_packages()
 		  )
+	
+	# For Excel (fill the cache so we can use constatns with late binding)
+	argv = ["makepy", r"C:\Program Files (x86)\Microsoft Office\Office14\Excel.exe"]
+	try:
+		makepy.main()
+	except:
+		argv= [""]
+		print "Choose Microsoft Excel 14.0 Object Library (1.7)"
+		makepy.main()
